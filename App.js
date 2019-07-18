8f9ca32a3ba9a495006cae2a9f6256198cc9f72e
timothydowd <timdowd1984@gmail.com>
2019-07-18 12:42:51 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>11+ Antonyms and Synonyms Learning and Test Aid</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
